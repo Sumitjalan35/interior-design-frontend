c86947681cc322e6c3dec62e9d2c9bd3377919ec
Sumitjalan35 <sumitjalan3534@gmail.com>
2025-07-24 23:40:32 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -16,7 +16,7 @@ const api = axios.create({
 // Request interceptor to add auth token
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('admin_token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
